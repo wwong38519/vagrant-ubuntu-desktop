431dd8cf5e96c413dccfde5feffe4da61e26e795
wwong38519 <wwong38519@gmail.com>
2016-01-17 00:34:35 +0800

    fix timezone script

--- provisioning/timezone.sh
+++ provisioning/timezone.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+sudo echo 'Asia/Hong_Kong' > /etc/timezone
 sudo rm /etc/localtime && sudo ln -s /usr/share/zoneinfo/Asia/Hong_Kong /etc/localtime
+sudo dpkg-reconfigure --frontend noninteractive tzdata
